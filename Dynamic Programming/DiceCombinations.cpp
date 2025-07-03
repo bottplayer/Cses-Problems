961f66e20286235a8d9b200ffa34f935b4ee829f
ganesh <ganyabachhav@gmail.com>
2025-07-04 01:24:52 +0530

    move ahead2

---
+++ Dynamic Programming/DiceCombinations.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+ll memo(ll n, vector<ll> &dp)
+{
+    if (n == 0)
+        return 1;
+    if (n < 0)
+        return 0;
+    if (dp[n] != -1)
+        return dp[n];
+
+    ll w = 0;
+    for (int i = 1; i <= 6; i++)
+        w = (w + memo(n - i, dp)) % mod;
+
+    return dp[n] = w;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<ll> dp(n + 1, -1);
+
+    cout << memo(n, dp) << endl;
+    return 0;
+}
